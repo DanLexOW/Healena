e0787b390e1866dd51c4447b23ce043d876e8041
Rina Mason <144337273+RinaGer@users.noreply.github.com>
2024-05-17 18:25:28 +0300

    Add files via upload

---
+++ Программирование/Healena_2/data_base/orm_query.py
@@ -0,0 +1,40 @@
+from sqlalchemy import select, update, delete
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from data_base.models import Training, Product, Uniduct
+
+
+async def orm_get_products(session: AsyncSession):
+    query = select(Product)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+async def orm_get_uniducts(session: AsyncSession):
+    query = select(Uniduct)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+async def orm_get_trainings(session: AsyncSession):
+    query = select(Training)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+async def orm_get_product(session: AsyncSession, product_name: str):
+    query = select(Product).where(Product.name == product_name)
+    result = await session.execute(query)
+    return result.scalar()
+
+
+async def orm_get_training(session: AsyncSession, training_id: int):
+    query = select(Training).where(Training.id == training_id)
+    result = await session.execute(query)
+    return result.scalar()
+
+
+
+
+
+
