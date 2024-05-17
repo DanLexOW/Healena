f00e6721405b932a2bbfdd4a9e98103e36002110
Rina Mason <144337273+RinaGer@users.noreply.github.com>
2024-05-17 18:09:43 +0300

    Add files via upload

---
+++ Программирование/Healena_1/bot_commands.py
@@ -0,0 +1,11 @@
+from aiogram.types import BotCommand
+
+private = {
+    #BotCommand(command="menu", description="Вот меню"),
+    #BotCommand(command="inline_keyboard", description="Клавиатура"),
+    BotCommand(command="start", description="Давайте начнем"),
+    BotCommand(command="enter_meal_statistics", description="Введите продукты с граммовкой и соотношением БЖУ, входящие в ваш прием пищи"),
+    BotCommand(command="report", description="Узнайте вашу 'калорийную' статистику за сегодня"),
+    #BotCommand(command="music", description="Музыку"),
+    #BotCommand(command="mood", description="Мне интересно узнать как вы себя чувствуете сегодня"),
+}
